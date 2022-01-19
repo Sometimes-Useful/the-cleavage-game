816011754464c7f06c47211f3721aec01ae3f786
Benjamin Bohec <bbohec.pro@gmail.com>
2022-01-19 21:46:03 +0100

    Updated Main Menu View

--- src/ui/views/MainMenu.svelte
+++ src/ui/views/MainMenu.svelte
@@ -1,15 +1,23 @@
 <script lang="ts">
+    import { InterfaceView } from "../../domain/entities/InterfaceView";
+    import { NavigateEvent } from "../../domain/events/navigateEvent/NavigateEvent";
     import { NewCleavageEvent } from "../../domain/events/newCleavage/NewCleavageEvent";
     import Button from "../components/button/button.svelte";
     import Subtitle from "../components/text/subtitle.svelte";
     import Title from "../components/text/title.svelte"
     import { applicationEventStore } from "../stores/stores";
-    const onClickButton = () => $applicationEventStore = new NewCleavageEvent()
 </script>
-<div class="flex flex-col w-full items-center">
+<div class="flex flex-col items-center">
     <Title/>
-    <Subtitle subtitle="Connectes-toi sur ta chaine Twitch pour faire participer tes viewers !!!"/>
+    <Subtitle subtitle="El famoso!
+    Le jeu du clivage où on décide enfin si quelque chose est de gauche ou de droite.
+    Un super jeu politique pour fight entre amis ou en famille.
+    Version streamer!
+    Tellement digital, tellement handspinner. Le plaisir.
+    Parfait pour préparer l'élection présidentielle de 2022!
+    4€99 seulement! Wouah!"/>
 </div>
-<div class="flex flex-col w-full items-center">
-    <Button text="Jouer!" size="large" onClick={onClickButton}/>
+<div class="flex flex-col items-center">
+    <Button text="Jouer !" size="large" onClick={()=>applicationEventStore.set(new NewCleavageEvent())}/>
+    <Button text="Options !" size="large" onClick={()=>applicationEventStore.set(new NavigateEvent(InterfaceView.SETTINGS))}/>
 </div>
