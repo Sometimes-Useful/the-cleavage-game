e4e9f7ac57dbbcb7a74d38531a7cd264687ce23c
Benjamin Bohec <bbohec.pro@gmail.com>
2022-01-19 21:45:31 +0100

    Studio view update

--- src/ui/views/Studio.svelte
+++ src/ui/views/Studio.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
-import { applicationStart } from "../../app";
-const onClick = () => applicationStart()
+    import { applicationStart } from "../../app";
 </script>
-<div class="flex flex-col w-full h-full items-center justify-evenly p-3 text-center" on:click={onClick}>
-    <div class="flex flex-col " >
-        <p class="text-white sm:text-lg text-sm  font-light">A game by</p>
-        <h1 class="text-white sm:text-5xl text-2xl sm:my-5 my-1">SOMETIMES USEFUL</h1>
-    </div>
-    <p class="text-white sm:text-lg text-sm  font-light">Inspired by Usul's game named "Le jeu du clivage".</p>
-    <h2 class="text-white sm:text-2xl text-xs font-light">{"<<<"} Click Anywhere to launch the game {">>>"}</h2>
+<div class="flex flex-col items-center w-full h-full justify-evenly p-3 text-center" on:click={() => applicationStart()}>
+    <h1 class="text-white sm:text-4xl text-lg">Le jeu du clivage</h1>
+    <p class="text-white sm:text-lg text-sm  font-light">A free open source game inspired by Usul's game named "Le jeu du clivage".</p>
+    <h2 class="text-white sm:text-2xl text-xs font-light animate-pulse ">{"<<<"} Click Anywhere to launch the game {">>>"}</h2>
 </div>
