ab3acd441f0f336f63ff254cec82b86cecb22ec0
Benjamin Bohec <bbohec.pro@gmail.com>
2022-05-25 11:18:11 +0200

    private key parsing with GCP secrets

--- src/env/serverEnvironnementVariables.ts
+++ src/env/serverEnvironnementVariables.ts
@@ -4,7 +4,7 @@ import { onMissingEnvVariable } from '../infra/EnvironmentVariable'
 config()
 export const gcpProjectId:string = process.env.GCP_PROJECT_ID ? process.env.GCP_PROJECT_ID : onMissingEnvVariable('process.env.GCP_PROJECT_ID')
 export const gcpClientEmail:string = process.env.GCP_CLIENT_EMAIL ? process.env.GCP_CLIENT_EMAIL : onMissingEnvVariable('process.env.GCP_CLIENT_EMAIL')
-export const gcpPrivateKey:string = process.env.GCP_PRIVATE_KEY ? process.env.GCP_PRIVATE_KEY : onMissingEnvVariable('process.env.GCP_PRIVATE_KEY')
+export const gcpPrivateKey:string = process.env.GCP_PRIVATE_KEY ? process.env.GCP_PRIVATE_KEY.replace(/\n/gm, '\n') : onMissingEnvVariable('process.env.GCP_PRIVATE_KEY')
 export const backendSheme:string = process.env.BACKEND_SHEME ? process.env.BACKEND_SHEME : onMissingEnvVariable('process.env.BACKEND_SHEME')
 export const backendFqdn:string = process.env.BACKEND_FQDN ? process.env.BACKEND_FQDN : onMissingEnvVariable('process.env.BACKEND_FQDN')
 export const backendPort = process.env.PORT
